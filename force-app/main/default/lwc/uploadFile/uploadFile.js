50402a2e424265df5f09805d9936e683d94ae77f
mrmanesh99 <rohitmanesh.c@ceptes.com>
2025-11-23 03:07:19 +0530

    Parsing csv is complete

--- force-app/main/default/lwc/uploadFile/uploadFile.js
+++ force-app/main/default/lwc/uploadFile/uploadFile.js
@@ -8,9 +8,32 @@ export default class UploadFile extends LightningElement {
         const reader=new FileReader();
         reader.onload=()=>{
             this.fileitems=reader.result;
+            console.log("FILE ITEMS"+this.fileitems)
+           this.csv= this.parseCSV(this.fileitems)
         }
         reader.readAsText(this.file);
+    
+}
+ parseCSV(csvText) {
+    const lines = csvText.split(/\r?\n/);  
+    const headers = lines[0].trim().split(',');
 
-        
+    let records = [];
 
-}}
+    for (let i = 1; i < lines.length; i++) {
+        if (!lines[i].trim()) continue;  // skip empty lines
+        let values = lines[i].split(',');
+        let obj = {};
+
+        headers.forEach((header, index) => {
+            obj[header.trim()] = values[index]?.trim();
+        });
+
+        records.push(obj);
+    }
+
+    return records;
+}
+
+
+}
